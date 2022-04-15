b42ba2c1252e9af9de20a2ebbfaa30108a10ca4c
daniaminakov <danilminakov333@gmail.com>
2022-04-15 12:24:01 +0300

    Init commit

---
+++ src/Parser.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace daniltest\parser;
+
+/**
+ * @author Danil
+ */
+class Parser implements ParserInterface {
+    /**
+     * @string $url
+     * @param string $tag
+     * @return array
+     */
+    
+    public function process(string $url, string $tag): array{
+        
+        $htmlPage = file_get_contents($url);
+        
+        if ($htmlPage === false) {
+            return ['Invalid URL'];
+        }
+        
+        preg_match('/<' . $tag . '.*?>(.*?)<\/' . $tag . '>/s', $htmlPage, $strings);
+        
+        if (empty($strings[1])) {
+            return ['There are no such tags on the page'];
+        }
+        
+        return $strings[1];
+    }
+}
---
+++ src/ParserInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @author Danil
+ */
+interface ParserInterface {
+
+    /**
+     * @param string $url
+     * @param string $tag
+     * @return array
+     */
+    public function process(string $url, string $tag): array;
+}
